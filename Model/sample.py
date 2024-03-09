1bd58b0b6e7c23ad64f3c185c90f102a751f2894
Sona Rosa Babloo <95519996+sonarosa@users.noreply.github.com>
2024-03-09 17:22:58 +0530

    Update sample.py

--- Model/sample.py
+++ Model/sample.py
@@ -1,9 +1,16 @@
 from IPython.display import Image, display
 from autogluon.multimodal import download
 import torchaudio
+from autogluon.multimodal import MultiModalPredictor
 
 url = "https://live.staticflickr.com/65535/49299665628_853f0bd470_w.jpg"
 anomaly_image = download(url)
 
 pil_img = Image(filename=anomaly_image)
 display(pil_img)
+predictor = MultiModalPredictor(problem_type="zero_shot_image_classification")
+prob = predictor.predict_proba({"image": [anomaly_image]}, {"text": ['Normal behavior',
+    'Suspicious activity',
+    'Unauthorized entry',
+    'Violent behavior']})
+print("Label probs:", prob)
